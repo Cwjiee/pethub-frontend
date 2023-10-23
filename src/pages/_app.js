f8495b55031c4323d71b84279cf7c6f33bb2aed5
Tharshen2124 <tharshensurianbalan@gmail.com>
2023-10-23 13:57:41 +0800

    feat: add nunito font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import { Inter, Nunito } from 'next/font/google'
+
+const inter = Nunito({
+  subsets: ["latin"],
+  weight: ["300", "500", "700"],
+});
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component  {...pageProps} />
+    </main>
+  ) 
 }
