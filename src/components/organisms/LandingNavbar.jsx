04360558ede586e99f0696a6a9e240fe3fc9946b
Wei Jie <jackchong398@gmail.com>
2023-10-31 11:55:44 +0800

    fix: modify landing

--- src/components/organisms/LandingNavbar.jsx
+++ src/components/organisms/LandingNavbar.jsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { v4 } from "uuid";
-import Logo from "../../../public/svg/purpleLogo.svg";
-import Pethub from "../../../public/svg/purplePethub.svg";
+import Pethub from "../../../public/svg/purplePethubLogo.svg"
 
 export default function LandingNavbar() {
   const links = [
@@ -26,18 +25,15 @@ export default function LandingNavbar() {
 
   return (
     <header>
-      <nav className="mx-auto w-[80%] flex items-center justify-between px-5 pt-[44px] text-white">
-        <div className="flex gap-x-2">
-          <Image src={Logo} alt="logo" width={34} height={34} />
-          <Image src={Pethub} alt="pethub" width={83} height={83} />
-        </div>
-        <div className="flex gap-x-12 justify-center items-center">
+      <nav className="mx-auto w-[95%] flex items-center justify-between px-5 pt-[44px] text-white">
+        <Image src={Pethub} alt="pethub logo" />
+        <div className="flex gap-x-12 justify-center items-center font-semibold">
           {links.map((link) => {
             return (
               <Link
                 key={v4()}
                 href={`${link.href}`}
-                className={`${link.label === "Login" && 'text-md relative border-[3px] border-white px-12 py-3 rounded-xl'}`}
+                className={`${link.label === "Login" && 'text-md relative border-[4px] border-white px-[50px] py-3 rounded-2xl'}`}
               >
                 <div>{link.label}</div>
               </Link>
