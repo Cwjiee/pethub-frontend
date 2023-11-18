4e7eca59ccd3e04a77107185cf53d075ab9e8b5a
Wei Jie <jackchong398@gmail.com>
2023-11-18 11:54:39 +0800

    feat: add admin footer

---
+++ src/components/organisms/AdminFooter.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import Logo from "../../../public/logo.svg"
+import Pethub from "../../../public/Pethub.svg"
+
+export default function AdminFooter() {
+  return (
+    <footer className="p-4 bg-white sm:p-6 mt-24">
+      <div className="mx-auto max-w-screen-xl">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <a href="https://flowbite.com" className="flex items-center">
+              <Image src={Logo} className="mr-3 h-8" alt="FlowBite Logo" width={50} height={50} />
+              <Image src={Pethub} alt="Pethub" width={80} height={80} />
+            </a>
+          </div>
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com" className="hover:underline">Pethub™</a>. All Rights Reserved.
+            </span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
--- src/pages/report/index.jsx
+++ src/pages/report/index.jsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/organisms/Navbar"
 import BackButton from "@/components/atoms/BackButton"
 import Table from "@/components/organisms/Table"
+import AdminFooter from "@/components/organisms/AdminFooter"
 
 export default function Report() {
   return (
@@ -8,13 +9,22 @@ export default function Report() {
       <Navbar></Navbar> 
       <div className="w-full m-auto pt-6 px-6">
         <BackButton/>
-        <div className="w-full mt-8 m-auto py-11 px-16 bg-white">
+        <div className="w-full mt-8 m-auto py-11 px-16">
           <div className="flex flex-col">
-            <div className="text-2xl">Report</div>
+            <div className="text-2xl mx-auto font-bold">Reports</div>
             <Table />
           </div>
         </div>
       </div>
     </>
+  )
+}
+
+Report.getLayout = function getLayout(page) {
+  return (
+    <main>
+      {page}
+      <AdminFooter />
+    </main>
   )
 }
