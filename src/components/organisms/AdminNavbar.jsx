c05adafa642e3ff7efb83b8b48dd9cd19a173322
weijie <jackchong398@gmail.com>
2024-02-16 01:22:34 +0800

    feat: sp navbar modal and logout logic

--- src/components/organisms/AdminNavbar.jsx
+++ src/components/organisms/AdminNavbar.jsx
@@ -1,6 +1,46 @@
+import { GlobalContext } from "@/context"
 import Image from "next/image"
+import { useContext } from "react"
+import { useToast } from "@chakra-ui/react"
+import { useRouter } from "next/router"
 
 export default function AdminNavbar() {
+  const url = process.env.NEXT_PUBLIC_API_URL
+  const { token } = useContext(GlobalContext)
+  const toast = useToast()
+  const router = useRouter()
+
+  const handleLogout = async () => {
+    const response = await fetch(`${url}/logout`, {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Accept": "application/json",
+      }
+    })
+
+    if (!response.ok) {
+      toast({
+        title: "failed to sign out",
+        description: 'Please try again',
+        status: 'error',
+        duration: 3000,
+        isClosable: true
+      })
+    } else {
+      toast({
+        title: "sign out successfully",
+        description: 'We will redirect back to landing page',
+        status: 'success',
+        duration: 3000,
+        isClosable: true
+      })
+      document.cookie = "roles=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+      sessionStorage.clear()
+      router.push('/')
+    }
+  }
+
   return (
     <>
       <header className="bg-white">
@@ -11,7 +51,7 @@ export default function AdminNavbar() {
           </div>
           <div className="flex gap-x-6 items-center">
             <div>Admin Mode</div>
-            <button className="flex justify-around px-12 py-3 rounded-[10px] bg-primary-500 hover:bg-primary-600 active:bg-primary-700">
+            <button onClick={handleLogout} className="flex justify-around px-12 py-3 rounded-[10px] bg-primary-500 hover:bg-primary-600 active:bg-primary-700">
               <div className="my-auto text-white font-bold spacing tracking-[0.86px] text-md">
                 Logout
               </div>
