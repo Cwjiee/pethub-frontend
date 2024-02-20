a79fe011da2ae4ae198beadca63bc8b9a642e080
Tharshen2124 <tharshensurianbalan@gmail.com>
2024-02-20 16:02:49 +0800

    fix: improve appointment history main page

--- src/components/organisms/AppointmentsTable.jsx
+++ src/components/organisms/AppointmentsTable.jsx
@@ -1,3 +1,5 @@
+import { TimeConvertNotForDateTime } from "@/utils/TimeConvertNotForDateTime"
+import { modDate } from "@/utils/modDate"
 import Link from "next/link"
 
 export default function AppointmentsTable({ appointments, token }) {
@@ -7,8 +9,8 @@ export default function AppointmentsTable({ appointments, token }) {
         <div class="mx-auto">
           <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             <div class="overflow-x-auto">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-black uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <table class="w-full text-md text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-sm font-bold text-black uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-4 py-4">Service Name</th>
                             <th scope="col" class="px-4 py-4">Appointment Date</th>
@@ -26,16 +28,16 @@ export default function AppointmentsTable({ appointments, token }) {
                         return (
                           <tr class="border-b dark:border-gray-700" key={appointment.appointment_id}>
                             <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{appointment.service_provider.full_name}</th>
-                            <td class="px-4 py-3 max-w-[12rem] truncate">{appointment.date}</td>
-                            <td class="px-4 py-3 max-w-[12rem] truncate">{appointment.time}</td>
+                            <td class="px-4 py-3 max-w-[12rem] truncate">{modDate(appointment.date)}</td>
+                            <td class="px-4 py-3 max-w-[12rem] truncate">{TimeConvertNotForDateTime(appointment.time)}</td>
                             <td class="px-4 py-3 max-w-[12rem] truncate">{appointment.appointment_type}</td>
                             <td class="px-4 py-3 max-w-[12rem] truncate font-bold">
-                              <span className={`${
-                                appointment.appointment_status === 'approved' ? 'text-green-600' :
-                                appointment.appointment_status === 'pending' ? 'text-yellow-400' :
-                                'text-red-600'
+                              <span className={`py-2 px-4 rounded-3xl ${
+                                appointment.appointment_status === 'approved' ? 'bg-[#22C55E] text-white ' :
+                                appointment.appointment_status === 'pending' ? 'bg-yellow-400 text-black' :
+                                'bg-red-600 text-white'
                               }`}>
-                                {appointment.appointment_status.toUpperCase()}
+                                {appointment.appointment_status}
                               </span>
                             </td>
                             <td class="px-4 py-3 flex items-center justify-end">
