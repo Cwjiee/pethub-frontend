ad53448051fad9462f1dcde624dd912a44425f80
weijie <jackchong398@gmail.com>
2024-02-20 22:27:47 +0800

    style: user and sp edit page bg colour

--- src/pages/service-providers/edit.jsx
+++ src/pages/service-providers/edit.jsx
@@ -7,17 +7,19 @@ import checkAuth from '@/utils/checkAuth';
 function ServiceProviderEditProfile() {
     return (
     <>
+      <div className='w-full bg-white'>
         <SPNavbar/>
-            <div className="w-[80%] h-full m-auto px-6 pt-10 flex flex-col gap-4">
-            <BackButton/>
-            <div className="text-3xl font-bold">
-                Edit Pet Service Provider Information
-            </div>
-                <ServiceProviderEditForm />
-            </div>
+        <div className="w-[80%] h-full m-auto px-6 pt-10 flex flex-col gap-4">
+          <BackButton/>
+          <div className="text-3xl font-bold">
+            Edit Pet Service Provider Information
+          </div>
+          <ServiceProviderEditForm />
+        </div>
         <SPFooter/>
+      </div>
     </>
-    )
+  )
 }
 
 ServiceProviderEditProfile.getLayout = function getLayout(page) {
