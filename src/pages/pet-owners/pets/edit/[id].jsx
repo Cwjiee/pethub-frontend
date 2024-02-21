728c3a590666fe73fbbdd696af3d0742eb87f99d
Tharshen2124 <tharshensurianbalan@gmail.com>
2024-02-21 19:24:07 +0800

    feat: return previous pet info to edit pet page

--- src/pages/pet-owners/pets/edit/[id].jsx
+++ src/pages/pet-owners/pets/edit/[id].jsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/organisms/Navbar";
 import { GlobalContext } from "@/context";
 import { useRouter } from "next/router";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useToast } from '@chakra-ui/react'
 import BackButton from "@/components/atoms/BackButton";
 import checkAuth from "@/utils/checkAuth";
 import Footer from "@/components/organisms/Footer";
+import LoadSpinner from "@/components/atoms/LoadSpinner";
 
 function EditPet() {
   const [petName, setPetName] = useState("");
@@ -20,6 +21,34 @@ function EditPet() {
   const router = useRouter();
   const id = router.query.id;
   const url = process.env.NEXT_PUBLIC_API_URL;
+  const [tokenReady, setTokenReady] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    (async () => {
+      if (tokenReady) {
+        const response = await fetch(`http://localhost/api/v1/pets/${id}`, {
+          headers: {
+            "Authorization": `Bearer ${token}`,
+            "Accept": "application/json"
+          }
+        })
+        const result = await response.json()
+        const pet = result.pet
+        setPetName(pet.pet_name)
+        setType(pet.type)
+        setBreed(pet.breed)
+        setAge(pet.age)
+        setDescription(pet.description)
+        console.log(result.pet);
+        setIsLoading(false)
+      }
+    })()
+  }, [tokenReady, id])
+
+  useEffect(() => {
+    if (token && id) setTokenReady(true)
+  }, [token, id])
 
 
   const uploadToClient = (e) => {
@@ -81,7 +110,7 @@ function EditPet() {
       }
     }
   }
-  return (
+  return !isLoading ? (
     <>
       <Navbar/>
       <div className="w-[90%] mx-auto m-10">
@@ -89,14 +118,15 @@ function EditPet() {
         <div className="bg-white rounded-lg shadow-lg mx-auto w-[530px] py-10 px-10 m-6">
 
           <h1 className="text-center text-2xl font-bold mb-5">Edit Pet</h1>
-          <form action="" className="text-sm" onSubmit={handleSubmit}>
+          <form action="" className="text-md" onSubmit={handleSubmit}>
             <div className="mt-3">
               <label htmlFor="pet-name">Pet Name: </label>
               <p>   
                 <input 
                   type="text" 
-                  className="rounded-lg w-full text-sm border-[#e0e0e0]"
+                  className="rounded-lg w-full text-md border-[#e0e0e0]"
                   onChange={(e) => {setPetName(e.target.value)}}
+                  value={petName}
                 />
               </p>
               {errors && errors.pet_name && <p className="text-red-500">{errors.pet_name.toString()}</p>}
@@ -106,19 +136,22 @@ function EditPet() {
               <p>
                 <input 
                   type="text" 
-                  className="rounded-lg w-full text-sm border-[#e0e0e0]"
+                  className="rounded-lg w-full text-md border-[#e0e0e0]"
                   onChange={(e) => {setType(e.target.value)}}
+                  placeholder="dog / cat / tortoise / snake"
+                  value={type}
                 />
               </p>
               {errors && errors.type && <p className="text-red-500">{errors.type.toString()}</p>}
             </div>
             <div className="mt-3">
-              <label htmlFor="breed">Breed: </label>
+              <label htmlFor="breed">Breed: <span class="text-md text-red-500">*Only for dogs and cats*</span></label>
               <p>
                 <input 
                   type="text" 
-                  className="rounded-lg w-full text-sm border-[#e0e0e0]"
+                  className="rounded-lg w-full text-md border-[#e0e0e0]"
                   onChange={(e) => {setBreed(e.target.value)}}
+                  value={breed}
                 />
               </p>
               {errors && errors.breed && <p className="text-red-500">{errors.breed.toString()}</p>}
@@ -128,8 +161,10 @@ function EditPet() {
               <p>  
                 <input 
                   type="text" 
-                  className="rounded-lg w-full text-sm border-[#e0e0e0]"
+                  className="rounded-lg w-full text-md border-[#e0e0e0]"
                   onChange={(e) => {setAge(e.target.value)}}
+                  placeholder={"7 years old / 6 months old"}
+                  value={age}
                 />
               </p>
               {errors && errors.age && <p className="text-red-500">{errors.age.toString()}</p>}
@@ -139,7 +174,7 @@ function EditPet() {
               <p>
                 <input 
                   type="file" 
-                  className="block w-full text-sm xtte-gray-900 border border-solid border-[#E1E1E1] rounded-lg cursor-pointer bg-transparent dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 file:rounded-xl file:p-2 file:bg-primary-500 hover:file:bg-primary-600 active:file:bg-primary-700"
+                  className="block w-full text-md xtte-gray-900 border border-solid border-[#E1E1E1] rounded-lg cursor-pointer bg-transparent dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 file:rounded-xl file:p-2 file:bg-primary-500 hover:file:bg-primary-600 active:file:bg-primary-700"
                   onChange={uploadToClient}
                 />
               </p>
@@ -153,8 +188,10 @@ function EditPet() {
                   id="" 
                   cols="30" 
                   rows="5" 
-                  className="text-sm w-full rounded-lg border-[#e0e0e0]"
+                  className="text-md w-full rounded-lg border-[#e0e0e0]"
                   onChange={(e) => {setDescription(e.target.value)}}
+                  placeholder="very active at sports..."
+                  value={description}
                 >
 
                 </textarea>
@@ -171,6 +208,8 @@ function EditPet() {
       </div>
       <Footer />
     </>
+  ) : (
+    <LoadSpinner />
   )
 }
 
