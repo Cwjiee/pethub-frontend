36433d6c175180f3eec60e7c9275d765bc1dc812
weijie <jackchong398@gmail.com>
2024-02-22 13:39:09 +0800

    style: resize news card

--- src/components/molecules/NewsBlock.jsx
+++ src/components/molecules/NewsBlock.jsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 import Link from "next/link";
-import NewsPlaceholder from "../../../public/png/NewsPlaceholder.png";
 
 export default function NewsBlock({ news }) {
   return (
-    <div className="sm:basis-[49%] h-80 rounded-[10px] bg-white shadow p-6 flex justify-between">
+    <div className="w-[80%] h-[30rem] rounded-[10px] bg-white shadow p-6 flex justify-between mx-auto">
       <img
         className="w-1/2 h-full"
         src={news.image}
@@ -12,7 +10,7 @@ export default function NewsBlock({ news }) {
       />
       <div className="w-1/2 flex flex-col pl-6 gap-[10px]">
         <h4 className="font-bold">{news.news_title}</h4>
-        <div className="mr-auto flex justify-center items-center">
+        <div className="mr-auto flex justify-center items-center gap-y-2">
           <div className="flex justify-left gap-x-2">
             {news.categories.map((category) => {
               return (
@@ -23,10 +21,10 @@ export default function NewsBlock({ news }) {
             )}
           </div>
         </div>
-        <div className="text-justify text-black text-sm font-normal">
+        <div className="text-justify text-black text-md font-normal overflow-auto h-[70%]">
           {news.news_description}
         </div>
-        <Link href={"news/" + news.news_id} className="mt-auto w-full flex justify-between items-center h-10 font-semibold rounded-[10px] shadow text-white text-sm bg-secondary-500 hover:bg-secondary-600 active:bg-secondary-700">
+        <Link href={"news/" + news.news_id} className="mt-auto w-full flex justify-between items-center h-12 font-semibold rounded-[10px] shadow text-white text-sm bg-secondary-500 hover:bg-secondary-600 active:bg-secondary-700">
           <div className="m-auto tracking-[0.753px]">Read More</div>
         </Link>
       </div>
