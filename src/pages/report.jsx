0ee4764aaecb6841e83fa7b83b9aaeebd112c864
weijie <jackchong398@gmail.com>
2024-02-22 22:23:58 +0800

    feat: report validation

--- src/pages/report.jsx
+++ src/pages/report.jsx
@@ -12,6 +12,26 @@ function Report() {
   const toast = useToast()
   const { token } = useContext(GlobalContext)
 
+  const toastMessage = (err) => {
+      toast({
+        title: err,
+        description: 'Please try again',
+        status: 'error',
+        duration: 3000,
+        isClosable: true
+      })
+  }
+
+  const clearRespectiveField = (err) => {
+    if (err === 'report_title') {
+      setTitle("")
+      return
+    } else if (err === 'report_description') {
+      setDescription("")
+      return
+    }
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault()
 
@@ -27,24 +47,24 @@ function Report() {
         }),
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
+          "Authorization": `Bearer ${token}`,
+          "Accept": "application/json"
         },
       });
 
       const result = await response.json()
       console.log(result.message)
 
-      setTitle("")
-      setDescription("")
-
       if(!response.ok) {
-        toast({
-          title: 'Failed to create report',
-          description: 'Please try again',
-          status: 'error',
-          duration: 3000,
-          isClosable: true
-        })
+        if (result.errors) {
+          Object.keys(result.errors).forEach((err) => {
+            const errMessage = result.errors[err]
+            clearRespectiveField(err)
+            toastMessage(errMessage)
+          })
+        } else {
+          toastMessage("Failed to create report")
+        }
       } else {
         toast({
           title: 'Success',
@@ -53,6 +73,9 @@ function Report() {
           duration: 3000,
           isClosable: true
         })
+        setTimeout(function () {
+          router.push('/healthcare-facility')
+        }, 1000)
       }
 
     } catch(err) {
@@ -74,6 +97,7 @@ function Report() {
               <input 
                 onChange={(e) => setTitle(e.target.value)}
                 type="text"
+                value={title}
                 className="w-full h-10 rounded-[10px] text-gray-900 px-4 p-2 block border border-neutral-200 focus:ring-blue-500 focus:border-blue-500"
               /> 
             </div>
@@ -82,6 +106,7 @@ function Report() {
               <textarea
                 id="message"
                 rows="4"
+                value={description}
                 class="block p-2.5 w-full h-52 text-gray-900 rounded-[10px] border border-neutral-200 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your content here..."
                 onChange={(e) => setDescription(e.target.value)}
               >
